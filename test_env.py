cf79b0cb5cb4aa37ae5f5838fef116937cd64577
WSJ-William <wangsijin0912@163.com>
2025-01-19 21:25:48 +0800

    Test Environment

---
+++ test_env.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+# Enter your OpenAI API Key here
+os.environ["OPENAI_API_KEY"] = ""
+llm = ChatOpenAI(model="gpt-4o")
+parser = StrOutputParser()
+
+
+test_template = PromptTemplate(
+    input_variables=[],
+    template="""
+        你是谁？
+    """
+)
+test_chain = test_template | llm | parser
+
+
+def test_env():
+    print(test_chain.invoke({}))
+
+
+
+if __name__=="__main__":
+    test_env()
